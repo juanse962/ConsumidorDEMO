9364bcae9bf68b1da23c17c65f04ccc92f76f6a8
juanse962 <juanseg11@hotmail.com>
2020-01-12 16:37:54 -0500

    Agregar archivos de proyecto.

---
+++ ConsumidorDEMO/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;  
+namespace ConsumidorDEMO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String cola = Convert.ToString(args[0]);
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel()) {
+
+                channel.QueueDeclare(cola,false,false,false,null);
+                var consumer = new EventingBasicConsumer(channel);
+                Console.WriteLine("Presiona CTRL + C para salir....");
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(cola, autoAck: true, consumer: consumer);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
